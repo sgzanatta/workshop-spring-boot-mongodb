5fb7b7f68a40053123e34dcf1af7547fc85f0852
Sérgio Zanatta <sgzanatta92@gmail.com>
2024-05-27 17:00:45 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/nelioalves/workshopmongo/resources/exception/StandardError.java
@@ -0,0 +1,70 @@
+package com.nelioalves.workshopmongo.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long timestamp;
+	private Integer status;
+	private String erro;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+		
+	}
+
+	public StandardError(Long timestamp, Integer status, String erro, String message, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.erro = erro;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getErro() {
+		return erro;
+	}
+
+	public void setErro(String erro) {
+		this.erro = erro;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+
+}
