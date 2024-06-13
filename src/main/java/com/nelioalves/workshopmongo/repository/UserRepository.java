794d338abc58b8c84577e5886a7d291de4b61f86
Sérgio Zanatta <sgzanatta92@gmail.com>
2024-06-13 17:05:52 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.nelioalves.workshopmongo.domain.User;
 
 public interface UserRepository extends MongoRepository<User, String>{
-	
+
 }
-
-
