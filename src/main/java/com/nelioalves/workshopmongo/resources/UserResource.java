500a5c8bcc5371bd2a1d5cf7aeed0163bc7d946b
Sérgio Zanatta <sgzanatta92@gmail.com>
2024-06-17 16:33:05 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -59,5 +59,11 @@ public class UserResource {
 		
 		return ResponseEntity.noContent().build();	
 	}
+	
+	@RequestMapping(value ="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List> findPosts(@PathVariable String id) {   
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
  
